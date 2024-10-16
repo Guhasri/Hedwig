817342c4e52838880f136a96bd14980f0a644f25
Guhasri R <118996341+Guhasri@users.noreply.github.com>
2024-10-16 10:21:29 +0530

    Update Email.js

--- backend/models/Email.js
+++ backend/models/Email.js
@@ -1,15 +1,17 @@
-// models/Email.js
 const mongoose = require('mongoose');
 
 const EmailSchema = new mongoose.Schema({
   to: { type: String, required: true },
   from: { type: String, required: true },
+  cc: { type: String },   // Added cc field
+  bcc: { type: String },  // Added bcc field
   subject: { type: String, required: true },
   body: { type: String, required: true },
   date: { type: Date, required: true },
   starred: { type: Boolean, default: false },
   bin: { type: Boolean, default: false },
   type: { type: String, default: 'inbox' },
+  attachments: { type: [String], default: [] }
 });
 
-module.exports = mongoose.model('Email', EmailSchema,'inbox');
+module.exports = mongoose.model('Email', EmailSchema, 'inbox');
